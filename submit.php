a4a0ceb9403e56bcee947528479f72b3cc5465e7
Silencelen <traxis@outlook.com>
2025-05-24 20:40:48 -0700

    a4.1 (multi OS post recording)

--- submit.php
+++ submit.php
@@ -2,14 +2,14 @@
 
 date_default_timezone_set('America/Los_Angeles');
 
-$name = isset($_POST['name']) ? trim(strip_tags(htmlspecialchars($_POST['name'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
-$email = isset($_POST['email']) ? trim(strip_tags(htmlspecialchars($_POST['email'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
-$phone = isset($_POST['phone']) ? trim(strip_tags(htmlspecialchars($_POST['phone'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
+$name    = isset($_POST['name']) ? trim(strip_tags(htmlspecialchars($_POST['name'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
+$email   = isset($_POST['email']) ? trim(strip_tags(htmlspecialchars($_POST['email'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
+$phone   = isset($_POST['phone']) ? trim(strip_tags(htmlspecialchars($_POST['phone'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
 $address = isset($_POST['address']) ? trim(strip_tags(htmlspecialchars($_POST['address'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'))) : '';
 
-if (strlen($name) > 100) $name = substr($name, 0, 100);
-if (strlen($email) > 320) $email = substr($email, 0, 320);
-if (strlen($phone) > 30) $phone = substr($phone, 0, 30);
+if (strlen($name) > 100)    $name    = substr($name, 0, 100);
+if (strlen($email) > 320)   $email   = substr($email, 0, 320);
+if (strlen($phone) > 30)    $phone   = substr($phone, 0, 30);
 if (strlen($address) > 500) $address = substr($address, 0, 500);
 
 if (!$name || (!$email && !$phone && !$address)) {
@@ -33,10 +33,20 @@ if ($phone) {
     $phone = '(' . substr($digits, 0, 3) . ') ' . substr($digits, 3, 3) . '-' . substr($digits, 6);
 }
 
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    $submissionsDir = 'C:/secure/submissions/';
+} else {
+    $submissionsDir = '/var/www/submissions/';
+}
+
+if (!is_dir($submissionsDir)) {
+    @mkdir($submissionsDir, 0755, true);
+}
+
 $timestamp = date('Ymd_His');
-$safeName = preg_replace('/[^A-Za-z0-9]/', '_', $name);
-$safeName = substr($safeName, 0, 50); // Avoid very long filenames
-$filename = 'C:/secure/submissions/' . "{$safeName}_{$timestamp}.txt";
+$safeName  = preg_replace('/[^A-Za-z0-9]/', '_', $name);
+$safeName  = substr($safeName, 0, 50);
+$filename  = $submissionsDir . "{$safeName}_{$timestamp}.txt";
 
 $content  = "Name:    {$name}\n";
 $content .= "Email:   {$email}\n";
